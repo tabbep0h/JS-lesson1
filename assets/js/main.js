00a5037ae98ba32b6104816d76d8e817bc42cf5e
tabbee <104757339+tabbep0h@users.noreply.github.com>
2022-11-16 09:31:46 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,187 @@
+     
+         //--Работа с переменными--//
+
+            /*let num = 3       //-- 1 Task
+            alert(num)
+
+            /*let a = 10        //-- 2 Task
+            let b = 2
+            alert(a + b);
+            alert(a - b);
+            alert(a * b);
+            alert(a / b);*/
+
+            /*let a = 15        //-- 3 Task
+            let b = 2
+            let result = a+b
+            alert(result)*/
+
+            /*let a = 10        //-- 4 Task
+            let b = 5
+            let c = 2
+            let num = a+b+c
+            alert(num)*/
+            
+            /*let a = 17;       //-- 5 Task
+            let b = 10;
+            let c = a - b;
+            let d = 7;
+            let result = c + d;
+            alert (result)*/
+
+        //--Работа со строками--//
+
+            /*let str = 'Привет,мир!'; //-- 1 Task
+            console.log(str)*/
+            
+            /*let str1 = 'Привет,';    //-- 2 Task
+            let str2 = 'мир!';    
+            let str = str1 + str2  
+            console.log(str)*/           
+
+            /*let name = "Тимур";      //-- 3 Task
+            alert(`Привет,${name}!`)*/                
+            
+            /*let age = 16; 
+            alert(`Мне ${age} лет!`)*/   //-- 4 Task
+            
+        //--Функция prompt--//
+
+            /*let name = prompt('Ваше имя:');
+            alert(`Ваше имя ${name}`)*/             //-- 1 Task
+        
+            /*let num = prompt('Введите число:');
+            alert(`Квадрат это числа = ${num**2}`)*/                           //-- 2 Task
+
+        //--Обращение к символам строки--//
+
+            /*let str = 'abcde'           //-- 1 Task
+            console.log(str[0]);
+            console.log(str[1]);
+            console.log(str[2]);
+            console.log(str[3]);
+            console.log(str[4]);*/
+                                       
+            // let num = 12345;           //-- 2 Task
+            // for (let i in num){
+            //     a = a * string(i)
+            //     console.log(num)
+            // }    
+
+        //--Практика--//
+        
+        // let hour = 60 * 60               //-- 1 Task
+        // let day = Number(hour) * 24 
+        // let month = Number(day) * 30 
+        // console.log(hour + "сек",day + "сек",month + "сек")
+
+        // let hour = "0" + 2;              //-- 2 Task
+        // let minute = 54;
+        // let second = "0" + 2
+        // console.log(hour + ":" + minute + ":" + second)
+
+        // let num = 10;                    //--3 Task
+        // let square = num * num 
+        // console.log(square)  
+        
+        //--Работа с присваиванием и декрементами--//
+
+        // let num = 47;                //--1 Task
+        // num += 7;
+        // num -= 18;
+        // num *= 10;
+        // num /= 15;
+        // console.log(num)
+
+        // let num = 10;
+        // num++;
+        // num++;
+        // num--;
+        // console.log(num)
+
+        //--Условные операторы--//
+
+        /*let a = 10;           //--Task 1 
+        if (a == 10){
+            alert('Верно');
+        }
+            else{
+                alert('Неверно');
+            }
+        */
+     
+        /*let message ;         //--Task 2 
+        let min = 60;
+
+        if (min >= 0 & min <= 25) {
+            message = 'Первая четверть часа';
+        } else if  (min >= 26 & min <= 30) {
+            message = 'Вторая четверь часа';
+        } else if  (min >= 31 & min <= 45) {
+            message = 'Третья четверь часа';
+        } else if (min >= 46 & min <= 59) {
+            message = 'Четвертая четверь часа';
+        } else if (min < 0 | min >= 60) {
+            message = 'чумба ты че сделал';
+        }
+
+        alert(message)*/
+
+        /*let lang = 'ru'         //--Task 3
+        let arr 
+
+        if (lang == 'ru') {
+            arr = ["Понедельник", 
+                " Вторник", 
+                " Среда",
+                " Четверг",
+                " Пятница",
+                " Суббота",
+                " Воскресенье"];
+        }
+            else if (lang == 'en') {
+                arr = ["Monday",
+                    " Tuesday",
+                    " Wednesday",	
+                    " Thursday",	
+                    " Friday",	
+                    " Saturday",	
+                    " Sunday"]
+            }
+            else{
+                arr = 'Некорректное значение'
+            }
+        alert(arr)*/
+
+        /*let a = '1';
+        let b = '1';
+        let type = typeof(b);
+
+        success = ( a == 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a > 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a < 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a >= 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a = 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a != 0 ) ? 'Верно':'Неверно';
+        alert(success);
+        success = ( a == 'test') ? 'Верно':'Неверно';
+        alert(success);
+        if (type == typeof(a) & a == b){
+            alert("Верно");}
+        else{
+            alert("Неверно")
+            }*/
+        
+       
+      
+    
+     
+        
+        
+
+
